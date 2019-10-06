1ab25ecd897ee30a2b23e785fa84f502e66e473f
hugo codinach <hugo.codinach@epitech.eu>
2019-10-06 20:10:27 +0200

    [EDIT] Map style

--- front/src/components/FindUs/FindUsComponent.js
+++ front/src/components/FindUs/FindUsComponent.js
@@ -3,43 +3,22 @@ import React, { Component } from 'react'
 
 //Style
 import "./FindUsComponent.css"
-import epitechLogo from '../../ressources/images/epitechMarker.png'
 
-//map
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import MapComponent from './MapComponent'
+
+const defaultViewPort = {
+    latitude: 43.608237,
+    longitude: 3.886071,
+    transitionDuration: 3000,
+    zoom: 13,
+}
 
 class FindUsComponent extends Component {
 
-    state = {
-        mapParam: {
-            lat: 43.608237,
-            lng: 3.886071,
-            zoom: 13,
-        }
-    }
-
     render() {
-        const { lat, lng, zoom } = this.state.mapParam;
-        const position = [lat, lng];
-
         return (
             <div className="findUsContainer">
-                <Map center={position} zoom={zoom} className="findUsMap">
-                    <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <Marker position={position}>
-                        <Popup>
-                            <div className='findUsPop'>
-                                <span className='findUsPopTitle'>{"{EPITECH MONTPELLIER}"}</span>
-                                <span className='findUsPopSubtitle'>3 place Paul Bec</span>
-                                <div className='findUsImgContainer'>
-                                    <img className='findUsEpitechLogo' alt='logo' src={epitechLogo}/>
-                                </div>
-                            </div>
-                        </Popup>
-                    </Marker>
-                </Map>
+                <MapComponent defaultViewPort={defaultViewPort} />
             </div>
         )
     }
