0fc5191b21084c28e3d91b3fa177daaab48c6974
hugocodinach <hugo.codinach@epitech.eu>
2020-05-26 23:18:05 +0200

    [BF] hgloves

--- hgloves/src/App.js
+++ hgloves/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Switch } from 'react-router-dom';
 import { MuiThemeProvider } from '@material-ui/core/styles';
+import { Router as DomRouter } from 'react-router-dom';
 
 //Style
 import theme from './styles/theme';
@@ -9,15 +10,20 @@ import theme from './styles/theme';
 //Components
 import Router from './components/router/Router';
 
+import { createBrowserHistory } from "history"
+const history = createBrowserHistory()
+
 function App() {
     return (
-        <div className="app" id='scrollDiv'>
-            <MuiThemeProvider theme={theme}>
-                <Switch>
-                    <Router />
-                </Switch>
-            </MuiThemeProvider>
-        </div>
+        <DomRouter history={history}>
+            <div className="app" id='scrollDiv'>
+                <MuiThemeProvider theme={theme}>
+                    <Switch>
+                        <Router />
+                    </Switch>
+                </MuiThemeProvider>
+            </div>
+        </DomRouter>
     );
 }
 
--- hgloves/src/index.js
+++ hgloves/src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Router } from "react-router-dom"
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createBrowserHistory } from "history"
-
-const history = createBrowserHistory()
 
 ReactDOM.render(
-    <Router history={history}>
-        <App />
-    </Router>
+    <App />
     , document.getElementById('root')
 );
 
