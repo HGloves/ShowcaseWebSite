241453f8dc2f76d175db39c91bf63e22ba3b1786
Antoine <antoine.laget@yahoo.fr>
2020-06-07 17:34:50 +0200

    fix(backend): correct return values

--- back/facebook/FacebookController.js
+++ back/facebook/FacebookController.js
@@ -7,11 +7,10 @@ let axios = require("axios")
 router.get('/facebook/feed/', function(req, res) {
     axios.get("https://graph.facebook.com/v7.0/HGlovesFTC/feed?access_token=2562126377364122|9Gk4eP4wWRBxeX442H8MHRQCnhA")
     .then(response => {
-        return response
+        return res.status(200).send(response);
     })
     .catch(error => {
-        console.log(error)
-        return
+        return res.status(400).send(error)
     })
 })
 
