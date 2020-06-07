5d1f2666b13859205ee1a8770f5a4903b0e289d4
Antoine <antoine.laget@yahoo.fr>
2020-06-07 15:48:56 +0200

    fix(facebook-backend): correct contact require

--- back/app.js
+++ back/app.js
@@ -6,7 +6,6 @@ const cors = require('cors')
 var ContactController = require('./contact/ContactController');
 var FacebookController = require("./facebook/FacebookController")
 
-const ContactController = require('./contact/ContactController');
 app.use(cors())
 app.use(ContactController);
 app.use(FacebookController);
